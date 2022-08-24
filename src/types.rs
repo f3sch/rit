274125ed79e366bb992f57fbe07840be320fc307
Felix Schlepper <f3sch.git@outlook.com>
2022-08-24 16:10:51 +0200

    added Object trait

--- src/types.rs
+++ src/types.rs
@@ -11,3 +11,14 @@ impl Types {
         }
     }
 }
+
+pub trait Object {
+    /// Return the type of the `Object`.
+    fn get_type(&self) -> Types;
+
+    /// Turn the data into its bytes.
+    fn get_data(&self) -> &Vec<u8>;
+
+    /// Set the `Object` id.
+    fn set_oid(&mut self, hash: String);
+}
