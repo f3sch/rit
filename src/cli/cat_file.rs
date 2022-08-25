d3414e7235b8a4f97da97e15d16878ccf4e9f6c4
Felix Schlepper <f3sch.git@outlook.com>
2022-08-25 10:57:40 +0200

    cat-file: cli: better description

--- src/cli/cat_file.rs
+++ src/cli/cat_file.rs
@@ -1,11 +1,11 @@
 use clap::Args;
 use std::path::PathBuf;
 
-/// cat-file command.
+/// Print some pretty information of a rit object.
 #[derive(Debug, Args)]
 #[clap(args_conflicts_with_subcommands = true)]
 pub struct CatFile {
-    /// Path to cat-file.
+    /// Path to the rit object.
     #[clap(value_parser)]
     pub path: PathBuf,
 }
