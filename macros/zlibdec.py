2da6dba8df8384de881801aa563fa3e71cb3b578
Felix Schlepper <f3sch.git@outlook.com>
2022-10-04 17:59:58 +0200

    macros: print proper oid

--- macros/zlibdec.py
+++ macros/zlibdec.py
@@ -11,14 +11,15 @@ path = argv[1]
 if not os.path.exists(path):
     print("Path does not exists!")
 elif os.path.isfile(path):
-    f = open(argv[1], 'rb')
+    f = open(argv[1], "rb")
     decomp = zlib.decompress(f.read())
     print(decomp)
 else:
     for root, _, files in os.walk(path):
         for file in files:
-            f = open(os.path.join(root,file), 'rb')
+            path = os.path.join(root, file)
+            f = open(path, "rb")
             decomp = zlib.decompress(f.read())
-            print("OID: {}".format(file))
+            print("OID: {}{}".format(path[-41:-39], path[-38:]))
             print(decomp)
             print("---")
